1f30a0e0b9c37c8817f7eb9a115c6d8d1ba73694
Ahassnat <alhassnatabdallah@gmail.com>
2021-01-09 00:44:07 +0200

    Create ROUTE module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 import { LoginComponent } from './Account/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginComponent } from './Account/login/login.component';
       LoginComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
